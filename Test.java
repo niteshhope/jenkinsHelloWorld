e705ec6f9573cc620be6e7dc4b3843565536bc7e
Nitesh <nitesh.hope@gmail.com>
2020-04-23 16:58:57 +0530

    update

--- Test.java
+++ Test.java
@@ -2,6 +2,6 @@ class Test
 { public static void main(String[] args)
 {
 System.out.println("****************Eat the Java world*************");
- System.out.println("***************trying webhook with jenkins*************");
+ System.out.println("***************trying webhook og github*************");
 }
  }
